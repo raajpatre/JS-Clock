5c7de3e09a8644a0b213647a565ce5f84b106925
raajpatre <raajpatre27@gmail.com>
2025-12-26 19:45:44 +0530

    Now the background changes color every second

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let hr=document.getElementById("hr")
 let min=document.getElementById("min")
 let sec=document.getElementById("sec")
+let bg=document.querySelector("main")
+
 function update(){
     let a= new Date();
     let h=a.getHours();
     let m=a.getMinutes();
     let s=a.getSeconds(); 
+    let r=Number.parseInt(Math.random()*257);
+    let g=Number.parseInt(Math.random()*257);
+    let b=Number.parseInt(Math.random()*257);
+    console.log(r,g,b);
     hr.innerHTML=h;
     min.innerHTML=m;
     sec.innerHTML=s;
+    bg.setAttribute(`style`,`background-color:rgb(${r},${g},${b})`);
 }
 
 setInterval(update,1000)
